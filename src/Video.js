1ab4d29a83a55069da22732076459dcb2a88546e
George Hoqqanen <george@hoqqanen.com>
2016-10-19 15:25:45 -0700

    Change video to next debate

--- src/Video.js
+++ src/Video.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Video extends React.Component {
   render() {
     return (
-      <iframe id="video" src="https://www.youtube.com/embed/h-gkBUbU_F4?autoplay=1" frameBorder="0"></iframe>
+      <iframe id="video" src="https://www.youtube.com/embed/smkyorC5qwc?autoplay=1" frameBorder="0"></iframe>
     );
   }
 }
